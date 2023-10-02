85081d3b5a96c417cee3bb91d8b6bae80d7588a1
salihdemirer-oredata <127302712+salihdemirer-oredata@users.noreply.github.com>
2023-10-02 15:17:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask.ext.mysql import MySQL
 import os
 
+mysql = MySQL()
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = os.getenv("mysql_user")
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("mysql_password")
+app.config['MYSQL_DATABASE_DB'] = os.getenv("db")
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("host")
+
 @app.route("/")
 def helloworld():
-    return "test5"
+    name = "demo"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    command = "insert into demo (name) values (%s)"
+    cursor.execute(command,name) 
+    conn.commit()
+    return str(name + " eklendi")
+
+    return "Second version"
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0')
+
