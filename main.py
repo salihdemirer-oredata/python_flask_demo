7dba081ddebc73549401fbb316e8b5ae02d100f0
Salih Furkan Demirer <salih.demirer@oredata.com>
2023-10-02 11:13:27 +0300

    updated main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello World!"
+    return "Second version"
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
