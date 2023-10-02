de7a3ef97f3efae919f18363cc055436d18be7a1
salihdemirer-oredata <127302712+salihdemirer-oredata@users.noreply.github.com>
2023-10-02 15:05:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
     return "test5"
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
