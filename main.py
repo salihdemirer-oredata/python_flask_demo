8f8f0e4b16c7cfc64af8ba14895a35ec46521696
Salih Furkan Demirer <salih.demirer@oredata.com>
2023-10-02 21:22:53 +0300

    uploaded new configuration

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+import os
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+ 
+mysql = MySQL(app)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=8080)
