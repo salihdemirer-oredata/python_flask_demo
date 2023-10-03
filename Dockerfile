a9832bfa04503ef550595dbc74339582fc4e79e4
Salih Furkan Demirer <salih.demirer@oredata.com>
2023-10-03 11:33:02 +0300

    updated requirements

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11.6
+FROM python:3.12
 WORKDIR /app
 COPY requirements.txt ./
 RUN pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
 import os
  
 app = Flask(__name__)
