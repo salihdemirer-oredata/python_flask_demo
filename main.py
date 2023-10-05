ef2dd630410718d6819a69032e08be197962dd71
Salih Furkan Demirer <salih.demirer@oredata.com>
2023-10-05 12:13:09 +0300

    updated main.py

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import os
 app = Flask(__name__)
 
 # PostgreSQL bağlantı bilgileri
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:test@127.0.0.1/test"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class Info(db.Model):
+    __tablename__ = 'info'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     age = db.Column(db.Integer, nullable=False)
